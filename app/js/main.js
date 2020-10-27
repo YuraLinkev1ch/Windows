fe5b01118d20f1c8e69e1612dd2df4700a2d5a94
yuralinkev1ch <yura.linkevich2012@gmail.com>
2020-10-27 17:02:56 +0300

    add glazing section

--- app/js/main.js
+++ app/js/main.js
@@ -34,4 +34,23 @@ im.mask(selector);
 /*modal-mask-end */
 
 
+/*glazing-tabs-start*/
 
+let tabs = document.querySelector('.container_glazing')
+let btns = tabs.querySelectorAll('.glazing__item')
+let items = tabs.querySelectorAll('.glazing-description-block')
+
+function change(arr, i) {
+	arr.forEach( item => {
+		item.forEach( i => {i.classList.remove('glazing_active')})
+		item[i].classList.add('glazing_active')
+	})
+}
+
+for(let i = 0; i < btns.length; i++) {
+	btns[i].addEventListener('click', () => {
+		change([btns, items], i)
+	})
+}
+
+/*glazing-tabs-end*/
